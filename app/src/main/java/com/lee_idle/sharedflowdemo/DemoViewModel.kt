9c17cdb3341f5ea868885b1d5d611e00784fef7c
Lee-IDLE <nidus0526@gmail.com>
2024-07-03 09:56:22 +0900

    SharedFlow를 공부하기 위한 데모 프로젝트입니다.

---
+++ app/src/main/java/com/lee_idle/sharedflowdemo/DemoViewModel.kt
@@ -0,0 +1,27 @@
+package com.lee_idle.sharedflowdemo
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+
+class DemoViewModel: ViewModel() {
+    private val _sharedFlow = MutableSharedFlow<Int>()
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+    init {
+        sharedFlowInit()
+    }
+
+    fun sharedFlowInit() {
+        viewModelScope.launch {
+            for (i in 1..1000) {
+                delay(2000)
+                println("Emitting $i")
+                _sharedFlow.emit(i)
+            }
+        }
+    }
+}
